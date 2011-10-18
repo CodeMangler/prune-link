b8c4af807cd1428beb67ce4798b427fbb724839e
CodeMangler <hsdpal@gmail.com>
2011-10-19 01:30:03 +0530

    Apparently, %{<name>} should be followed by a format specified, like so: %{<name>}s for the string formatting operator..

--- prune/utils/constants.py
+++ prune/utils/constants.py
@@ -1,7 +1,7 @@
 __author__ = 'CodeMangler'
 
-USER_AGENT_REQUEST_URL = 'http://www.useragentstring.com/?uas=%(UAS)&getJSON=all'
-GEOLOCATION_REQUEST_URL = 'http://api.ipinfodb.com/v2/ip_query.php?key=0bde88d75043de5ab285b843b0a682cf8a50687e21e3a39d2e82cba582456509&ip=%(IP)&output=json&timezone=false'
+USER_AGENT_REQUEST_URL = 'http://www.useragentstring.com/?uas=%(UAS)s&getJSON=all'
+GEOLOCATION_REQUEST_URL = 'http://api.ipinfodb.com/v2/ip_query.php?key=0bde88d75043de5ab285b843b0a682cf8a50687e21e3a39d2e82cba582456509&ip=%(IP)s&output=json&timezone=false'
 SHORT_LINK_LENGTH = 6 # Sufficient for 44261653680 (62P6) URLs. If we run out, we can simply increase this, provided we haven't placed any constraints on the DB field.
 AGGREGATE_LINK_LENGTH = 8
 
